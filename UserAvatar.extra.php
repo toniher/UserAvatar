3632d4f362391e9743667fb35de5f8c4e12779ef
Toni Hermoso Pulido <toniher@cau.cat>
2014-09-27 11:03:35 +0200

    update making composer compatible

--- UserAvatar.extra.php
+++ UserAvatar.extra.php
@@ -4,42 +4,69 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
 
-/** REGISTRATION */
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'UserAvatar',
-	'version' => '0.1',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:UserAvatar',
-	'author' => array( 'Toniher' ),
-	'descriptionmsg' => 'useravatar_desc',
-);
+//self executing anonymous function to prevent global scope assumptions
+call_user_func( function() {
 
-#http://www.mediawiki.org/wiki/Manual:$wgResourceModules
-$wgResourceModules['ext.UserAvatar'] = array(
-	'localBasePath' => __DIR__,
-	'scripts' => array( 'js/ext.UserAvatar.js' ),
-	'styles' => array( 'css/ext.UserAvatar.css' ),
-	'remoteExtPath' => 'UserAvatar'
-);
+	/** REGISTRATION */
+	$GLOBALS['wgExtensionCredits']['parserhook'][] = array(
+		'path' => __FILE__,
+		'name' => 'UserAvatar',
+		'version' => '0.1',
+		'url' => 'https://www.mediawiki.org/wiki/Extension:UserAvatar',
+		'author' => array( 'Toniher' ),
+		'descriptionmsg' => 'useravatar_desc',
+	);
+	
+	#http://www.mediawiki.org/wiki/Manual:$wgResourceModules
+	$GLOBALS['wgResourceModules']['ext.UserAvatar'] = array(
+		'localBasePath' => __DIR__,
+		'scripts' => array( 'js/ext.UserAvatar.js' ),
+		'styles' => array( 'css/ext.UserAvatar.css' ),
+		'remoteExtPath' => 'UserAvatar'
+	);
 
-/** LOADING OF CLASSES **/
-// https://www.mediawiki.org/wiki/Manual:$wgAutoloadClasses
-$wgAutoloadClasses['UserAvatar'] = __DIR__ . '/UserAvatar.classes.php';
-$wgAutoloadClasses['ApiUserAvatar'] = dirname( __FILE__ ). '/UserAvatar.api.php';
+	/** LOADING OF CLASSES **/
+	// https://www.mediawiki.org/wiki/Manual:$wgAutoloadClasses
+	$GLOBALS['wgAutoloadClasses']['UserAvatar'] = __DIR__ . '/UserAvatar.classes.php';
+	$GLOBALS['wgAutoloadClasses']['ApiUserAvatar'] = dirname( __FILE__ ). '/UserAvatar.api.php';
 
-// API module
-$wgAPIModules['useravatar'] = 'ApiUserAvatar';
+	// API module
+	$GLOBALS['wgAPIModules']['useravatar'] = 'ApiUserAvatar';
 
-/** STRINGS AND THEIR TRANSLATIONS **/
-$wgExtensionMessagesFiles['UserAvatar'] = __DIR__ . '/UserAvatar.i18n.php';
-$wgExtensionMessagesFiles['UserAvatarMagic'] = __DIR__ . '/UserAvatar.i18n.magic.php';
+	/** STRINGS AND THEIR TRANSLATIONS **/
+	$GLOBALS['wgExtensionMessagesFiles']['UserAvatar'] = __DIR__ . '/UserAvatar.i18n.php';
+	$GLOBALS['wgExtensionMessagesFiles']['UserAvatarMagic'] = __DIR__ . '/UserAvatar.i18n.magic.php';
 
-/** HOOKS **/
+	/** HOOKS **/
+	
+	#http://www.mediawiki.org/wiki/Manual:Hooks/ParserFirstCallInit
+	#http://www.mediawiki.org/wiki/Manual:Tag_extensions
+	$GLOBALS['wgHooks']['ParserFirstCallInit'][] = 'UserAvatarSetupTagExtension';
+	
+	#http://www.mediawiki.org/wiki/Manual:Parser_functions
+	$GLOBALS['$wgHooks']['ParserFirstCallInit'][] = 'UserAvatarSetupParserFunction';
+	
+	#http://www.mediawiki.org/wiki/Manual:Hooks/SkinAfterContent
+	# We put avatar at the end of articles created by one guy.
+	$GLOBALS['wgHooks']['SkinAfterContent'][] = 'UserAvatar::onSkinAfterContent';
+	
+	#http://www.mediawiki.org/wiki/Manual:Hooks/OutputPageParserOutput
+	# We put avatar only on User Page
+	$GLOBALS['wgHooks']['OutputPageParserOutput'][] = 'UserAvatar::onOutputPageParserOutput';
+	
+	#http://www.mediawiki.org/wiki/Manual:Hooks/OutputPageBeforeHTML
+	# We add this for loading CSS and JSS in every page by default
+	$GLOBALS['wgHooks']['OutputPageBeforeHTML'][] = 'UserAvatar::onOutputPageBeforeHTML';
 
-#http://www.mediawiki.org/wiki/Manual:Hooks/ParserFirstCallInit
-#http://www.mediawiki.org/wiki/Manual:Tag_extensions
-$wgHooks['ParserFirstCallInit'][] = 'UserAvatarSetupTagExtension';
+	#http://www.mediawiki.org/wiki/Manual:Hooks/PageHistoryLineEnding
+	# We add avatar to the history listing
+	$GLOBALS['wgHooks']['PageHistoryLineEnding'][] = 'UserAvatar::onPageHistoryLineEnding';
 
+	#Ajax
+	#https://www.mediawiki.org/wiki/Manual:$wgAjaxExportList
+	$GLOBALS['wgAjaxExportList'][] = 'UserAvatar::getUserInfo';
+
+});
 
 // Hook our callback function into the parser
 function UserAvatarSetupTagExtension( $parser ) {
@@ -51,10 +78,6 @@ function UserAvatarSetupTagExtension( $parser ) {
 	return true;
 }
 
-#http://www.mediawiki.org/wiki/Manual:Parser_functions
-$wgHooks['ParserFirstCallInit'][] = 'UserAvatarSetupParserFunction';
-
-
 // Hook our callback function into the parser
 function UserAvatarSetupParserFunction( $parser ) {
 	// When the parser sees the {{#userprofile:}} function, it executes 
@@ -65,24 +88,5 @@ function UserAvatarSetupParserFunction( $parser ) {
 	return true;
 }
 
-#http://www.mediawiki.org/wiki/Manual:Hooks/SkinAfterContent
-# We put avatar at the end of articles created by one guy.
-$wgHooks['SkinAfterContent'][] = 'UserAvatar::onSkinAfterContent';
-
-#http://www.mediawiki.org/wiki/Manual:Hooks/OutputPageParserOutput
-# We put avatar only on User Page
-$wgHooks['OutputPageParserOutput'][] = 'UserAvatar::onOutputPageParserOutput';
-
-#http://www.mediawiki.org/wiki/Manual:Hooks/OutputPageBeforeHTML
-# We add this for loading CSS and JSS in every page by default
-$wgHooks['OutputPageBeforeHTML'][] = 'UserAvatar::onOutputPageBeforeHTML';
-
-#http://www.mediawiki.org/wiki/Manual:Hooks/PageHistoryLineEnding
-# We add avatar to the history listing
-$wgHooks['PageHistoryLineEnding'][] = 'UserAvatar::onPageHistoryLineEnding';
-
-#Ajax
-#https://www.mediawiki.org/wiki/Manual:$wgAjaxExportList
-$wgAjaxExportList[] = 'UserAvatar::getUserInfo';
 
 
